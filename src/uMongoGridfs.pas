b9207baef4fec1bd3aaa30dfe9f830803362cf67
alexeyvo <alexeyvo@softwareplanet.uk.com>
2015-01-09 05:08:47 -0600

    Use Delphi's set instead bit manipulations

--- src/uMongoGridfs.pas
+++ src/uMongoGridfs.pas
@@ -5,12 +5,9 @@ interface
 uses
   uMongo, MongoBson, uMongoGridfsFile, uDelphi5;
 
-const
-   MONGOC_CNV_NONE        = 0;
-   MONGOC_CNV_COMPRESS    = 1 shl 1;
-   MONGOC_CNV_ENCRYPT     = 1 shl 2;
-   MONGOC_CNV_UNCOMPRESS  = 1 shl 3;
-   MONGOC_CNV_DECRYPT     = 1 shl 4;
+type
+  TMongoFlag = (mfCompress, mfEncrypt, mfUncompress, mfDecrypt);
+  TMongoFlags = set of TMongoFlag;
 
 type
   EMongoGridfs = class(EMongo);
@@ -19,14 +16,14 @@ type
     ['{c78da0ba-6665-4aa1-9c27-f47a1f3eda41}']
     procedure Drop;
     function CreateFile(const AName: UTF8String;
-                        AFlags: Integer = MONGOC_CNV_NONE;
+                        AFlags: TMongoFlags = [];
                         const AMetadata: IBson = nil;
                         const AContentType: UTF8String = ''): IMongoGridfsFile;
     function GetFileNames: TStringArray;
     function FindFile(const AQuery: IBson = nil;
-                      AFlags: Integer = MONGOC_CNV_NONE): IMongoGridfsFile; overload;
+                      AFlags: TMongoFlags = []): IMongoGridfsFile; overload;
     function FindFile(const AName: UTF8String;
-                      AFlags: Integer = MONGOC_CNV_NONE): IMongoGridfsFile; overload;
+                      AFlags: TMongoFlags = []): IMongoGridfsFile; overload;
     procedure RemoveFile(const AName: UTF8String);
   end;
 
@@ -42,19 +39,20 @@ type
   private
     FNativeGridfs: Pointer;
     FError: bson_error_t;
+    function FlagsToNative(AFlags: TMongoFlags): Integer;
   public
     constructor Create(ANativeGridfs: Pointer);
     destructor Destroy; override;
     procedure Drop;
     function CreateFile(const AName: UTF8String;
-                        AFlags: Integer = MONGOC_CNV_NONE;
+                        AFlags: TMongoFlags;
                         const AMetadata: IBson = nil;
                         const AContentType: UTF8String = ''): IMongoGridfsFile;
     function GetFileNames: TStringArray;
     function FindFile(const AQuery: IBson = nil;
-                      AFlags: Integer = MONGOC_CNV_NONE): IMongoGridfsFile; overload;
+                      AFlags: TMongoFlags = []): IMongoGridfsFile; overload;
     function FindFile(const AName: UTF8String;
-                      AFlags: Integer = MONGOC_CNV_NONE): IMongoGridfsFile; overload;
+                      AFlags: TMongoFlags): IMongoGridfsFile; overload;
     procedure RemoveFile(const AName: UTF8String);
   end;
 
@@ -65,7 +63,7 @@ begin
   FNativeGridfs := ANativeGridfs;
 end;
 
-function TMongoGridfs.CreateFile(const AName: UTF8String; AFlags: Integer;
+function TMongoGridfs.CreateFile(const AName: UTF8String; AFlags: TMongoFlags;
   const AMetadata: IBson; const AContentType: UTF8String): IMongoGridfsFile;
 var
   nativeFile: Pointer;
@@ -81,7 +79,8 @@ begin
       content_type := PAnsiChar(AContentType);
   end;
 
-  nativeFile := mongoc_gridfs_create_cnv_file(FNativeGridfs, @opt, AFlags);
+  nativeFile := mongoc_gridfs_create_cnv_file(FNativeGridfs, @opt,
+                                              FlagsToNative(AFlags));
   Result := NewMongoGridfsFile(nativeFile);
 end;
 
@@ -98,22 +97,35 @@ begin
 end;
 
 function TMongoGridfs.FindFile(const AName: UTF8String;
-  AFlags: Integer): IMongoGridfsFile;
+  AFlags: TMongoFlags): IMongoGridfsFile;
 var
   nativeFile: Pointer;
   err: bson_error_t;
 begin
   nativeFile := mongoc_gridfs_find_one_cnv_by_filename(FNativeGridfs,
                                                        PAnsiChar(AName),
-                                                       @err, AFlags);
+                                                       @err, FlagsToNative(AFlags));
   if nativeFile = nil then
     raise EMongoGridfs.Create(@err);
 
   Result := NewMongoGridfsFile(nativeFile);
 end;
 
+function TMongoGridfs.FlagsToNative(AFlags: TMongoFlags): Integer;
+begin
+  Result := MONGOC_CNV_NONE;
+  if mfCompress in AFlags then
+    Result := Result or MONGOC_CNV_COMPRESS;
+  if mfEncrypt in AFlags then
+    Result := Result or MONGOC_CNV_ENCRYPT;
+  if mfUncompress in AFlags then
+    Result := Result or MONGOC_CNV_UNCOMPRESS;
+  if mfDecrypt in AFlags then
+    Result := Result or MONGOC_CNV_DECRYPT;
+end;
+
 function TMongoGridfs.FindFile(const AQuery: IBson;
-  AFlags: Integer): IMongoGridfsFile;
+  AFlags: TMongoFlags): IMongoGridfsFile;
 var
   nativeFile: Pointer;
   err: bson_error_t;
@@ -125,7 +137,8 @@ begin
     query := AQuery;
 
   nativeFile := mongoc_gridfs_find_one_cnv(FNativeGridfs,
-                                           query.NativeBson, @err, AFlags);
+                                           query.NativeBson, @err,
+                                           FlagsToNative(AFlags));
   if nativeFile = nil then
     raise EMongoGridfs.Create('File not found');
 
