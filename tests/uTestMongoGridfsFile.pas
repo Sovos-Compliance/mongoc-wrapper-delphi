8ee70e7b7896dd0e8a7be70608948ec8a9f0dd45
Jose Sebastian Battig <jsbattig@convey.com>
2015-07-27 09:25:29 -0500

    B-0 added test to check file not found behavior

--- tests/uTestMongoGridfsFile.pas
+++ tests/uTestMongoGridfsFile.pas
@@ -25,6 +25,7 @@ type
     procedure Write_Read;
     procedure Compressed;
     procedure Encrypted;
+    procedure Open_FileNotFound;
     procedure WriteAndReadBackSomeChunks;
     procedure WriteAndReadBackSomeChunksTryBoundaries;
     procedure StressWriteReads;
@@ -170,6 +171,12 @@ begin
 
   FFile := FGridfs.FindFile;
   CheckEquals(Length(TEST_DATA), FFile.Read(FBuf, SizeOf(FBuf)));
+end;
+
+procedure TestMongoGridfsFile.Open_FileNotFound;
+begin
+  //CreateFileStub('test', TEST_DATA);
+  FFile := FGridfs.FindFile;
 end;
 
 procedure TestMongoGridfsFile.Seek;
